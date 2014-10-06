b697dca3a00da938405d12b99f3b176e35533b70
knorbi5 <knorbi8@gmail.com>
2014-10-06 22:33:10 +0200

    Téma alapok

---
+++ footer.php
@@ -0,0 +1,10 @@
+		<footer>
+			<div id="footeritemsContainer">
+				<div class="footeritem cPointer fselected"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons/yt.png"></div>
+				<div class="footeritem cPointer"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons/fb.png"></div>
+				<div class="footeritem cPointer"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons/phone.png"></div>
+				<div class="footeritem cPointer"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons/mail.png"></div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title><?php wp_title(); ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/style.css">
+    </head>
+	<body>
+		<div id="mainmenu">
+			<img id="mainmenu_image" class="cPointer" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.jpg">
+			<div id="menuitemsContainer" class="cPointer">
+				<div class="menuitem cPointer <!--hselected-->">ÓRAREND</div>
+				<div class="menuitem cPointer">TANÁROK</div>
+				<div class="menuitem cPointer">GALÉRIA</div>
+				<div class="menuitem cPointer">HELYSZÍNEK</div>
+				<div class="menuitem cPointer">KAPCSOLAT</div>
+			</div>
+		</div>
