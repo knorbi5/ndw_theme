c007df04a3c9b0fce2955b31f05ec830882ac7d8
knorbi5 <knorbi8@gmail.com>
2014-10-19 00:16:21 +0200

    Főoldal cikkmegjelenítő + alsó sáv

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
 	get_header();
 	
-	$mainArticleID = get_cat_ID('vezercikk');
-	
-	$args = array('category'=>$mainArticleID, 'posts_per_page'=>1);
+	$args = array('category_name'=>'vezercikk', 'posts_per_page'=>1);
 	$posts = get_posts($args);
 ?>
 	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.0";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	<script type="text/javascript">
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.0";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	
+		jQuery(document).ready(function(){
+			jQuery(".ndwPostViewerPost").click(function(){
+				jQuery("#ndwPostViewerSelectedImage").attr("src", jQuery(this).attr("data-fullImageSRC"));
+			});
+		});
+	</script>
 	<div id="content">
 		<div class="fb-like-box" style="position: absolute" data-href="https://www.facebook.com/NewDanceWorld?fref=ts" data-width="280" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="true"></div>
 		<div id="mainArticle">
@@ -34,6 +40,25 @@
 				echo $post->post_title;
 			}
 ?>
+		</div>
+		<div>Kiemelt cikkek:</div>
+		<div id="ndwPostViewer">
+			<div id="ndwPostViewerScroller">
+<?php
+				$args = array('category_name'=>'kiemelt', 'posts_per_page'=>-1);
+				$posts = get_posts($args);
+				
+				foreach($posts as $post){
+?>
+					<div class="ndwPostViewerPost" data-fullImageSRC="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full')[0]; ?>"><img src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail')[0]; ?>"><?php echo $post->post_title; ?></div>
+<?php
+				}
+?>
+			</div>
+			<img id="ndwPostViewerSelectedImage" src="">
+		</div>
+		<div id="indexFooter">
+		
 		</div>
 	</div>
 <?php
